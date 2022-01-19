a343ff69a12041fb58dfdfab8d32f822523d2dcf
Kamil Kaczmarczyk <kizeenn@gmail.com>
2022-01-19 12:31:34 +0100

    Remove create idparams

--- src/pages/identities/create.tsx
+++ src/pages/identities/create.tsx
@@ -1,10 +1,8 @@
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
 import Create from "~/components/identities/identity/create/index";
 
 const IdentityPage: NextPage = () => {
-  const router = useRouter();
-  return <Create id={router.query.id} />;
+  return <Create />;
 };
 
 export default IdentityPage;
